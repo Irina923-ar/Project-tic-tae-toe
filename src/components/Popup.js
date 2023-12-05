79f95f84f91e0008cca1f0d3e1d1acbdb954183e
Galan Irinae <galanirinad@gmail.com>
2023-12-05 17:11:36 +0200

    update popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -14,19 +14,26 @@ function Popup({ winner, onQuit, onNextRound, playerMark }) {
   return (
     <div className="popup">
       <div className="popup-winner">
-        <div className="popup-title fs-200 fw-bold text-secondary-300 bg-secondary-200">{getMessage()}</div>
+        <div className="popup-title fs-200 fw-bold text-secondary-300 bg-secondary-200">
+          {getMessage()}
+        </div>
         <div className="popup-subtitle fs-500 fw-bold text-primary-100 bg-secondary-200">
           {winner ? `${winner} TAKES THE ROUND` : "ROUND TIED"}
         </div>
-        <button onClick={() => onQuit(false)} className="btn-1 btn-quit bg-secondary-300 fs-200 fw-bold">
-          QUIT
-        </button>
-        <button
-          /* onClick={onNextRound(false)} */
-          className="btn-2 btn-next-round bg-primary-300 fs-200 fw-bold"
-        >
-          NEXT ROUND
-        </button>
+        <div>
+          <button
+            onClick={() => onQuit(false)}
+            className="btn-1 btn-quit bg-secondary-300 fs-200 fw-bold"
+          >
+            QUIT
+          </button>
+          <button
+            /* onClick={onNextRound(false)} */
+            className="btn-2 btn-next-round bg-primary-300 fs-200 fw-bold"
+          >
+            NEXT ROUND
+          </button>
+        </div>
       </div>
     </div>
   );
